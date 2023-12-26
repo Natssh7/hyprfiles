a3bed83d32473541cef4cccd72460b1032c501b4
Natssh7 <127946775+Natssh7@users.noreply.github.com>
2023-12-26 22:28:08 +0100

    Delete .config directory for cleanup

--- .config/thorium/Default/Extensions/mmhlniccooihdimnnjhamobppdhaolme/3.11.13_0/dist/assets/SessionType-21786bb4.js
+++
@@ -1,8 +0,0 @@
-var SessionType = /* @__PURE__ */ ((SessionType2) => {
-  SessionType2["Event"] = "event";
-  SessionType2["Websocket"] = "websocket";
-  return SessionType2;
-})(SessionType || {});
-export {
-  SessionType as S
-};
